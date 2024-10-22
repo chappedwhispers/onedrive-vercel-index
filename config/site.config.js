23d8fa0bb38ac2b25f46a32fe6bc2c4a3f24fce7
chappedwhispers <67685030+chappedwhispers@users.noreply.github.com>
2024-10-22 20:32:17 +0200

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -31,9 +31,9 @@ module.exports = {
   // googleFontSans - the sans serif font used in onedrive-vercel-index.
   googleFontSans: 'Plus Jakarta Sans',
   // googleFontMono - the monospace font used in onedrive-vercel-index.
-  googleFontMono: 'JetBrains Mono',
+  googleFontMono: 'Reddit Mono',
   // googleFontLinks -  an array of links for referencing the google font assets.
-  googleFontLinks: ['https://fonts.googleapis.com/css2?family=JetBrains+Mono&family=Plus+Jakarta+Sans:wght@400;500;700&display=swap'],
+  googleFontLinks: ['https://fonts.googleapis.com/css2?family=Reddit+Mono&family=Plus+Jakarta+Sans:wght@400;500;700&display=swap'],
 
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
